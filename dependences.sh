539154b176f93f8b63c5155f1f94ed5606fbbf73
Andreffelipe <felipe.andre35@yahoo.com.br>
2022-01-17 18:29:37 -0300

    adicionado arquivo dependences.sh para configurar o projeto

---
+++ dependences.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+GIT_NAME="meu-git"
+
+echo "inicializando o go.mod"
+go mod init github.com/${GIT_NAME}/go_task_cli_mongo
+
+echo "criando variável ambiente"
+echo DATABASE_URL= > .env
+
+echo "instalando pacote do mongo"
+go get go.mongodb.org/mongo-driver
+
+echo "instalando pacote para cli"
+go get github.com/urfave/cli/v2
